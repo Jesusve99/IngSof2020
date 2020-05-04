c3cfd1e05ee22832c3b06ca0ef82dd2da7e1a2b8
Jesusve99 <58119012+Jesusve99@users.noreply.github.com>
2020-05-04 17:06:42 +0200

    BD

---
+++ NBP/src/modelo/BD.java
@@ -0,0 +1,139 @@
+package modelo;
+
+import java.sql.*;
+import java.util.*;
+
+//USER:rWiO7EseHz
+//PASS:vKa9rl8nd5
+//https://remotemysql.com/phpmyadmin/db_structure.php?db=rWiO7EseHz
+
+public class BD 
+{
+	
+	private Connection con ;	
+	
+	public BD()
+	{
+		try
+		{
+			try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException ex) {
+                System.out.println("Error al registrar el driver de MySQL: " + ex);
+            }
+			String jdbcUrl = "jdbc:mysql://remotemysql.com:3306/rWiO7EseHz";
+
+			con = DriverManager.getConnection(jdbcUrl, "rWiO7EseHz", "vKa9rl8nd5");
+		}
+		catch (SQLException ex)
+		{
+			throw new BDException("Error al Conectar con la base de datos." + ex.getMessage());
+		}
+	}
+	
+	protected void finalize () 
+	{
+		try
+		{
+			if (con!=null)  con.close();
+		}
+		catch (SQLException ex)
+		{
+			throw new BDException("Error al Cerrar la Conexi�n." + ex.getMessage());
+		}
+    }
+	
+	public Object SelectEscalar(String sel)
+	{
+		ResultSet rset;
+		Object res = null;
+		try
+		{
+			Statement stmt = con.createStatement();
+			rset = stmt.executeQuery(sel);
+			rset.next();
+			res = rset.getObject(1);
+			rset.close();
+			stmt.close();
+		}
+		catch (SQLException ex)
+		{
+			throw new BDException("Error en el SELECT: " + sel + ". " + ex.getMessage());
+		}		
+		
+		return res;
+	}
+	
+	public List<Object[]> Select(String sel)
+	{
+		ResultSet rset;
+		List<Object[]> lista = new ArrayList<Object[]>();
+		try
+		{
+			Statement stmt = con.createStatement();
+			rset = stmt.executeQuery(sel);
+			ResultSetMetaData meta = rset.getMetaData();
+			int numCol = meta.getColumnCount();
+			while (rset.next())
+			{
+				Object[] tupla = new Object[numCol];
+				for(int i=0; i<numCol;++i)
+				{
+					tupla[i] = rset.getObject(i+1);
+				}
+				lista.add(tupla);
+			}
+			rset.close();
+			stmt.close();
+		}
+		catch (SQLException ex)
+		{
+			throw new BDException("Error en el SELECT: " + sel+ ". " + ex.getMessage());
+		}		
+		
+		return lista;
+	}
+	
+	public void Insert(String ins)
+	{
+		try
+		{
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(ins);
+			stmt.close();
+		}
+		catch (SQLException ex)
+		{
+			throw new BDException("Error en el INSERT: " + ins+ ". " + ex.getMessage());
+		}
+	}
+
+	public void Delete(String del)
+	{
+		try
+		{
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(del);
+			stmt.close();
+		}
+		catch (SQLException ex)
+		{
+			throw new BDException("Error en el DELETE: " + del+ ". " + ex.getMessage());
+		}
+	}
+
+	public void Update(String up)
+	{
+		try
+		{
+			Statement stmt = con.createStatement();
+			stmt.executeUpdate(up);
+			stmt.close();
+		}
+		catch (SQLException ex)
+		{
+			throw new BDException("Error en el UPDATE: " + up+ ". " + ex.getMessage());
+		}
+	}
+
+}
