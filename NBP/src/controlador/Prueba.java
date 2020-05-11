5fbddec83341362aba74cc398f3d471e03508a3f
Jesusve99 <58119012+Jesusve99@users.noreply.github.com>
2020-05-11 17:09:22 +0200

    Mensaje al correo + lib

--- NBP/src/controlador/Prueba.java
+++ NBP/src/controlador/Prueba.java
@@ -2,12 +2,39 @@ package controlador;
 
 import java.util.List;
 
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+
 import modelo.BD;
-import modelo.Jugador;
+
+//Correo:ingsoftwarenbp@gmail.com
+//Contraseña:vKa9rl8nd5
 
 public class Prueba {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws EmailException {
+		//bd();
+		enviar("Que pasa fiera :)","marialopez57@uma.es");	
+	}
+	
+	public static void enviar (String str,String destino) throws EmailException {
+		Email email = new SimpleEmail();
+		email.setHostName("smtp.gmail.com");
+		email.setSmtpPort(465);
+		email.setSSLOnConnect(true);
+		email.setAuthenticator(new DefaultAuthenticator("ingsoftwarenbp@gmail.com", "vKa9rl8nd5"));
+		email.setSSLOnConnect(true);
+		email.setFrom("ingsoftwarenbp@gmail.com");
+		email.setSubject("TestMail");
+		email.setMsg(str);
+		email.addTo(destino);
+		email.send();
+		System.out.println("Mensaje enviado");
+	}
+	
+	public static void bd() {
 		BD mibd = new BD();
 		
 		List<Object[]> lista = mibd.Select("SELECT * FROM Jugador WHERE correo = 'prueba@';");
@@ -17,9 +44,6 @@ public class Prueba {
 		
 		System.out.println(email);
 		System.out.println(contrasena);
-
-		
-		
 	}
 
 }
