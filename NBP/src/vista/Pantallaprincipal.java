799030b366222143111de0783efe6cb4dd3201e6
adricamu <adriancamara09@uma.es>
2020-05-18 19:46:01 +0200

    Inicio y registrarse sin terminar

--- NBP/src/vista/Pantallaprincipal.java
+++ NBP/src/vista/Pantallaprincipal.java
@@ -6,10 +6,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Pantallaprincipal extends JFrame {
-
-	private JPanel contentPane;
 
 	/**
 	 * Launch the application.
@@ -33,10 +37,6 @@ public class Pantallaprincipal extends JFrame {
 	public Pantallaprincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
 	}
 
 }
