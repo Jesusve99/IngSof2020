4df472d469a22a1f8ba52a476af074a7428c8d23
mari57 <62023773+mari57@users.noreply.github.com>
2020-05-23 21:30:53 +0200

    ..

--- NBP/src/modelo/Partido.java
+++ NBP/src/modelo/Partido.java
@@ -6,6 +6,8 @@ import java.time.LocalTime;
 import java.util.Date;
 import java.util.Locale;
 
+
+
 import com.mysql.cj.protocol.a.MysqlBinaryValueDecoder;
 import com.mysql.cj.result.LocalTimeValueFactory;
 
