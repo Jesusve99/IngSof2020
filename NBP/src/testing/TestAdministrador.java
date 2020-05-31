9adf4139a82107cf38f90de990fe0629e54377e6
Rabana21 <rbnbullones1@uma.es>
2020-05-31 18:13:27 +0200

    Update TestAdministrador.java

--- NBP/src/testing/TestAdministrador.java
+++ NBP/src/testing/TestAdministrador.java
@@ -1,5 +1,7 @@
 package testing;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.Before;
 import modelo.Administrador;
@@ -7,10 +9,11 @@ import org.junit.jupiter.api.Test;
 
 class TestAdministrador {
 	private Administrador adminPrueba;
-	
+	private Administrador adminNoRegistrado;
 	@Before
 	public void init() {
 		adminPrueba = new Administrador("prueba@gmail.com", "Prueba2000", "Málaga");
+		adminNoRegistrado = new Administrador("estoNoEsUnCorreoRegistrado@gmail.com", "Contraseña1234");
 	}
 	
 	@Test
@@ -20,12 +23,40 @@ class TestAdministrador {
 		adminPrueba.agregarAdministrador();
 		int numAdministradorPostAgregacion = (int) Administrador.getTotalAdministrador();
 		assertEquals(numAdministradoresPrincipio+1, numAdministradorPostAgregacion);
-		//Prueba de que no agrega Adminiistrradores con correo a NULL
+		//Prueba de que no agrega Administrradores con correo a NULL
 		Administrador adminIncorrecto = new Administrador(null, null);
 		adminIncorrecto.agregarAdministrador();
-		assertEquals(numAdministradorPostAgregacion, Administrador.getTotalAdministrador());
+		assertEquals(numAdministradorPostAgregacion, Administrador.getTotalAdministrador());		
+	}
 	
+	@Test
+	void testCorreoRegistrado() {
+		//Comprobamos que el correo que esta registrado con un correo ya agregado
+		adminPrueba.agregarAdministrador();
+		assertTrue(adminPrueba.correoRegistrado());
+		
+		//Comprobamos que el correo no esta registrado con un correo inventado
+		assertFalse(adminNoRegistrado.correoRegistrado());
+	}
+	
+	
+	@Test
+	void testInicioCorrecto() {
+		//Comprobamos que los datos esta registrado con un Administrador ya agregado
+		adminPrueba.agregarAdministrador();
+		assertTrue(adminPrueba.datosInicioCorrecto());
+		
+		//Comprobamos que los datos esta registrado con un Administrador inventado
+		assertFalse(adminNoRegistrado.datosInicioCorrecto());
+	}
+	
+	@Test
+	void testEliminarAdministrador() {
+		adminPrueba.agregarAdministrador();
+		int numAdministradores = (int) Administrador.getTotalAdministrador();
+		adminPrueba.eliminarAdministrador();
+		assertEquals(numAdministradores-1, Administrador.getTotalAdministrador());
+		
 		
 	}
-
 }
